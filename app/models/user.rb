8ac1acc1572648f5f2f85eab1add7f04dabced83
EC2 Default User <ec2-user@ip-172-31-42-10.ec2.internal>
2021-07-01 14:19:11 +0000

    ユーザー編集バリデーションのrender先修正　テスト完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,6 @@ class User < ApplicationRecord
   has_many :books
   attachment :profile_image, destroy: false
 
-  validates :name,
-  presence: true,
-  length: {maximum: 20, minimum: 2}, uniqueness: true
-
-  validates :introduction,
-  length: {maximum: 50}
+  validates :name, presence: true, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :introduction, length: {maximum: 50}
 end
